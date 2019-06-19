3d8dc91dac5e548f5b6288bec6b6795e111faf27
yanchanllin <yonalin1@gmail.com>
2019-06-18 23:55:31 -0700

    update books controller index to include error and render bookserializer

--- book-fellow-backend/app/controllers/api/v1/books_controller.rb
+++ book-fellow-backend/app/controllers/api/v1/books_controller.rb
@@ -3,7 +3,12 @@ class Api::V1::BooksController < ApplicationController
     def index
         if logged_in?
             @books = current_user.books    
-        render json: @books
+        render json: BookSerializer.new(@books)
+        else 
+            render json: {
+                error: "Must be logged in to see trips"
+            }
       end
+    end 
 
 end

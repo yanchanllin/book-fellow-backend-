7da3f0e571481fbfc326dfd69da33cb61451940c
yanchanllin <yonalin1@gmail.com>
2019-06-18 23:56:11 -0700

    change book serializer alittle

--- book-fellow-backend/app/serializers/book_serializer.rb
+++ book-fellow-backend/app/serializers/book_serializer.rb
@@ -1,4 +1,15 @@
 class BookSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :author, :description
+
+  attribute :reviews do |book|
+    book.reviews.map do |rev|
+      {
+        content: rev.content,
+        rating: rev.rating
+      }
+    end
+  end
+
+  
 end

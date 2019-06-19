5a75842d1fce761a0944635f9c939b5c035eb5e6
yanchanllin <yonalin1@gmail.com>
2019-06-18 22:25:48 -0700

    update user serializer to display book description

--- book-fellow-backend/app/serializers/user_serializer.rb
+++ book-fellow-backend/app/serializers/user_serializer.rb
@@ -1,6 +1,12 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username, :name
-  has_many :books
-  
+  # has_many :books, serializer: BookSerializer
+
+  attribute :books do |user|
+    {
+      books: user.books
+    
+    }
+  end
 end

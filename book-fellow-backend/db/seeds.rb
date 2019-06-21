2a42f0b977bd9cac3249f8ce19e8f69444206b09
yanchanllin <yonalin1@gmail.com>
2019-06-21 11:41:34 -0700

    add new book for user lilil

--- book-fellow-backend/db/seeds.rb
+++ book-fellow-backend/db/seeds.rb
@@ -7,11 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# lili = User.first
+lili = User.first
 # #  User.create(name:"Amy", username:"Amyl", password:"password")
 #  amy = User.second
 #  amy.books.create(name: "Blue", author:"Chris Lam", description:"It's good for age 4 & up")
  
-# #  lili.books.create(name: "Bad Dad", author:"David Walliams", description:"age 8 & up,Gangsters, poop, lots of heart in zany British bestseller")
+ lili.books.create(name: "The Tiny Seed", author:"Eric Carle", description:"age 4 & up,Its journey is perilous, and the reader learns the fate of fellow seeds")
 # #  lili.reviews.create(content:"good for beginnner reading", rating:"4/5", user_id:1,book_id:1)
 # amy.reviews.create(content:"Each page is packed with intriguing facts, future places, and lots more!", rating:"4/5",user_id:1,book_id:2)

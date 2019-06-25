50a2262a9276e7d57c41f3098dde5a55dafe2e54
yanchanllin <yonalin1@gmail.com>
2019-06-25 13:22:04 -0700

    updated reviews controller for index and show

--- book-fellow-backend/app/controllers/api/v1/reviews_controller.rb
+++ book-fellow-backend/app/controllers/api/v1/reviews_controller.rb
@@ -1,8 +1,19 @@
 class Api::V1::ReviewsController < ApplicationController
 
     def index
-        @reviews = Review.all
-    
-        render json: @reviews
+
+        if logged_in?
+            @reviews = current_user.book.reviews    
+        render json: ReviewSerializer.new(@reviews)
+        else 
+            render json: {
+                error: "Must be logged in to see reviews"
+            }
       end
+    end 
+
+    def show 
+        render json: @review
+    end 
+        
 end

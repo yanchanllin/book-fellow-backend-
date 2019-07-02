a70354dafbabb694659b45e21247ea1be0a7cb91
yanchanllin <yonalin1@gmail.com>
2019-07-02 13:50:59 -0700

    update reviews controller

--- book-fellow-backend/app/controllers/api/v1/reviews_controller.rb
+++ book-fellow-backend/app/controllers/api/v1/reviews_controller.rb
@@ -13,15 +13,36 @@ class Api::V1::ReviewsController < ApplicationController
     end 
 
     def show 
-    
         review_json = ReviewSerializer.new(@review).serialized_json
         render json:review_json
     end 
         
+    def create
+        @review = Review.new(review_params)
+     
+        if @review.save
+            render json: @review, status: :created, location: @review
+       else
+         render json: @review.errors, status: :unprocessable_entity
+       end
+     end
+    
+     def update
+        if @review.update(review_params)
+          render json: @review
+        else
+          render json: @review.errors, status: :unprocessable_entity
+        end
+      end
+
     private
 
+      def set_review
+        @review = Review.find(params[:id])
+      end
+
       def review_params
-        params.require(:review).permit(:content, :rating)
+        params.require(:review).permit(:content, :rating, :user_id, :book_id)
       end
 
   end

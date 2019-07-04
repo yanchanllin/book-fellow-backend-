cd4fbf31e2b4e95643805060c2e9d34fb0bd3667
yanchanllin <yonalin1@gmail.com>
2019-07-03 19:00:55 -0700

    update mybooks reducer

--- book-fellow-frontend/src/reducers/myBooks.js
+++ book-fellow-frontend/src/reducers/myBooks.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case "SET_MY_BOOKS":
       return action.books;
+    case "ADD_BOOK":
+      return state.concat(action.book);
     case "CLEAR_BOOKS":
       return [];
     default:

0431044b6b7037f428a4cc0db7af23b73afd6c29
yanchanllin <yonalin1@gmail.com>
2019-07-03 19:05:52 -0700

    update reviewform component to submit

---
+++ book-fellow-frontend/src/components/ReviewForm.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import { connect } from "react-redux";
+import { updateNewReviewForm } from "../actions/reviewForm.js";
+import { createReview } from "../actions/reviewForm.js";
+
+const ReviewForm = ({
+  formData,
+  history,
+  updateNewReviewForm,
+  createReview,
+  bookId
+}) => {
+  const { content, rating } = formData;
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    updateNewReviewForm(name, value);
+  };
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  createReview({
+    ...formData,
+    bookId
+  });
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="content"
+        name="content"
+        onChange={handleChange}
+        value={content}
+      />
+      <br />
+      <input
+        placeholder="rating"
+        name="rating"
+        onChange={handleChange}
+        value={rating}
+      />
+      <br />
+      <input type="submit" value="Create Review" />
+    </form>
+  );
+};
+
+const mapStateToProps = state => {
+  const bookId = state.book ? state.book.id : "";
+  return {
+    formData: state.reviewForm,
+    bookId
+  };
+};
+export default connect(
+  mapStateToProps,
+  { updateNewReviewForm, createReview }
+)(ReviewForm);

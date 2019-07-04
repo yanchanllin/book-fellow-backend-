f3e8a40da28d7f739fefa2b195b5498cf8d89b37
yanchanllin <yonalin1@gmail.com>
2019-07-03 18:59:40 -0700

    update book card component

--- book-fellow-frontend/src/components/BookCard.js
+++ book-fellow-frontend/src/components/BookCard.js
@@ -3,15 +3,19 @@ import ReviewCard from "./ReviewCard.js";
 // import ReviewForm from "./ReviewForm.js";
 
 const BookCard = ({ book }) => {
-  return (
+  return book ? (
     <div>
+      <h3> Title: {book.attributes.name} </h3>
       <li>
-        Title:{book.attributes.name} by:
-        {book.attributes.author} <br />[ {book.attributes.description}]
-        <ReviewCard reviews={book.attributes.reviews} />
-        {/* <ReviewForm /> */}
+        by: {book.attributes.author} <br />
+        [description: {book.attributes.description}]
       </li>
+      Review: <ReviewCard reviews={book.attributes.reviews} />
+      {/* <ReviewForm /> */}
+      <br />
     </div>
+  ) : (
+    <p>This the the BOOK card with no BOOK!</p>
   );
 };
 

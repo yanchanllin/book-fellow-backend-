443b28103a885bd531e7376a325d834e25c21660
yanchanllin <yonalin1@gmail.com>
2019-07-03 18:54:11 -0700

    update book controller create

--- book-fellow-backend/app/controllers/api/v1/books_controller.rb
+++ book-fellow-backend/app/controllers/api/v1/books_controller.rb
@@ -12,19 +12,37 @@ class Api::V1::BooksController < ApplicationController
     end 
 
      def show
-        book_json = BookSerializer.new(@book).serialized_json
-        render json:book_json
+        render json: @book
       end
 
     def create
-        @book = Book.new(book_params)
-        current_user.books << @book
-        @book.save
-        flash[:notice] = 'Book was successfully created.'
-        render json:@books
+    @book = Book.new(book_params)
+
+    if @book.save
+      render json: BookSerializer.new(@books), status: :created
+    else
+      error_resp = {
+        error: @book.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
     end
+  end
+
+     
+    def update
+        if @book.update(book_params)
+          render json: @book
+        else
+          render json: @book.errors, status: :unprocessable_entity
+        end
+      end
 
     private
+
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
     def book_params
       params.require(:book).permit(
           :name,

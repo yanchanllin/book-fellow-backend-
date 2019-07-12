6a6cbd9077bbcafaba78fa623d2d42bbc22f873e
yanchanllin <yonalin1@gmail.com>
2019-07-11 19:12:44 -0700

    add delete book

--- book-fellow-frontend/src/components/BookCard.js
+++ book-fellow-frontend/src/components/BookCard.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ReviewCard from "./ReviewCard.js";
 // import ReviewForm from "./ReviewForm.js";
+import { deleteBookSuccess } from "../actions/myBooks";
 
-const BookCard = ({ book }) => {
+const BookCard = ({ book, deleteBookSuccess }) => {
   return book ? (
     <div>
       <h3> Title: {book.attributes.name} </h3>
@@ -15,10 +16,18 @@ const BookCard = ({ book }) => {
       {/* <ReviewForm /> */}
       <br />
       <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
+      <br />
+      <Link to={`/books`}>DELETE this book</Link>
     </div>
   ) : (
     <p>This the the BOOK card with no BOOK!</p>
   );
+};
+
+const bookCard = {
+  marginTop: "20px",
+  marginLeft: "-110px",
+  width: "120%"
 };
 
 export default BookCard;

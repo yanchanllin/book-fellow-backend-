fd74f0f5d7e3faecff62c171111ef5dc2ac55294
yanchanllin <yonalin1@gmail.com>
2019-07-16 10:30:52 -0700

    add update and delete link to book card

--- book-fellow-frontend/src/components/BookCard.js
+++ book-fellow-frontend/src/components/BookCard.js
@@ -1,34 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import ReviewCard from "./ReviewCard.js";
 // import ReviewForm from "./ReviewForm.js";
-import { deleteBookSuccess } from "../actions/myBooks";
+// import { deleteBookSuccess } from "../actions/myBooks";
 
-const BookCard = ({ book, deleteBookSuccess }) => {
-  return book ? (
-    <div>
-      <h3> Title: {book.attributes.name} </h3>
-      <li>
-        by: {book.attributes.author} <br />
-        [description: {book.attributes.description}]
-      </li>
-      Review: <ReviewCard reviews={book.attributes.reviews} />
-      {/* <ReviewForm /> */}
-      <br />
-      <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
-      <br />
-      <Link to={`/books`}>DELETE this book</Link>
-    </div>
-  ) : (
-    <p>This the the BOOK card with no BOOK!</p>
-  );
-};
+class BookCard extends Component {
+  render() {
+    const { book, deleteBook } = this.props;
+    console.log(this.props);
+    return book ? (
+      <div>
+        <h3> Title: {book.attributes.name} </h3>
+        <li>
+          by: {book.attributes.author} <br />
+          [description: {book.attributes.description}]
+        </li>
+        Review: <ReviewCard reviews={book.attributes.reviews} />
+        {/* <ReviewForm /> */}
+        <br />
+        <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
+        <br />
+        <button onClick={() => this.props.deleteBook(book.id)}>DELETE</button>
+      </div>
+    ) : (
+      <p>This the the BOOK card with no BOOK!</p>
+    );
+  }
+}
 
-const bookCard = {
-  marginTop: "20px",
-  marginLeft: "-110px",
-  width: "120%"
-};
+// const bookCard = {
+//   marginTop: "20px",
+//   marginLeft: "-110px",
+//   width: "120%"
+// };
 
 export default BookCard;
 

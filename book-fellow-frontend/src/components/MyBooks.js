c8dd70c38764c481e32306efee1f514a8aaf6276
yanchanllin <yonalin1@gmail.com>
2019-07-17 14:31:20 -0700

    add card styling for my books

--- book-fellow-frontend/src/components/MyBooks.js
+++ book-fellow-frontend/src/components/MyBooks.js
@@ -2,19 +2,47 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { deleteBook } from "../actions/myBooks.js";
+import Card from "react-bootstrap/Card";
 
 const MyBooks = (props, deleteBook) => {
   console.log(props);
   const bookCards =
     props.books.length > 0
       ? props.books.map(b => (
-          <p key={b.id} b={b} deleteBook={props.deleteBook}>
-            <Link to={`/books/${b.id}`}>{b.attributes.name}</Link>
-          </p>
+          <>
+            <Card
+              key={b.id}
+              b={b}
+              deleteBook={props.deleteBook}
+              style={{
+                width: "16rem"
+              }}
+            >
+              <Card.Body>
+                <Card.Title>{b.attributes.name}</Card.Title>
+                <Card.Text>by: {b.attributes.author}</Card.Text>
+                <Link to={`/books/${b.id}`}>Read more</Link>
+              </Card.Body>
+            </Card>
+            <br />
+          </>
         ))
       : null;
 
-  return bookCards;
+  return (
+    <div>
+      <h1 align="center">All Books</h1>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between"
+        }}
+      >
+        {bookCards}
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {

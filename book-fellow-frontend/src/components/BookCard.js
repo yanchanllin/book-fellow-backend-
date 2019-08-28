5c87ea257d0143d1d524591047be2cb77a8e06c5
yanchanllin <yonalin1@gmail.com>
2019-08-27 23:44:38 -0700

    updated BookCard

--- book-fellow-frontend/src/components/BookCard.js
+++ book-fellow-frontend/src/components/BookCard.js
@@ -1,31 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import ReviewCard from "./ReviewCard.js";
 // import ReviewForm from "./ReviewForm.js";
 
-class BookCard extends Component {
-  render() {
-    const { book, deleteBook } = this.props;
-    console.log(this.props);
-    return book ? (
-      <div>
-        <h3> Title: {book.attributes.name} </h3>
-        <li>
-          by: {book.attributes.author} <br />
-          [description: {book.attributes.description}]
-        </li>
-        Review: <ReviewCard reviews={book.attributes.reviews} />
-        {/* <ReviewForm /> */}
-        <br />
-        <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
-        <br />
-        <button onClick={() => this.props.deleteBook(book.id)}>DELETE</button>
-      </div>
-    ) : (
-      <p>This the the BOOK card with no BOOK!</p>
-    );
-  }
-}
+const BookCard = ({ book, deleteBook }) => {
+  return book ? (
+    <div>
+      <h3> Title: {book.attributes.name} </h3>
+      <li>
+        by: {book.attributes.author} <br />
+        [description: {book.attributes.description}] Review:{" "}
+        <ReviewCard reviews={book.attributes.reviews} />
+      </li>
+      <br />
+      <Link to={`/books/${book.id}/edit`}>Edit this book</Link>
+      <br />
+      <button onClick={() => deleteBook(book.id)}>DELETE</button>
+      
+    </div>
+  ) : (
+    <p>This the the BOOK card with no BOOK!</p>
+  );
+};
 
 // const bookCard = {
 //   marginTop: "20px",

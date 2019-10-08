9c235482592017add5750372a237ffa8c6061817
yanchanllin <yonalin1@gmail.com>
2019-10-07 19:43:25 -0700

    delete spaces

--- book-fellow-backend/app/controllers/api/v1/users_controller.rb
+++ book-fellow-backend/app/controllers/api/v1/users_controller.rb
@@ -1,23 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
   def index
     @users = User.all
     render json: @users 
   end
 
-  # GET /users/1
   def show
-    # render json: @user
-    # rails api status codes 
-
-    # json_string = MovieSerializer.new(movie).serialized_json
     user_json = UserSerializer.new(@user).serialized_json
     render json:user_json
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
 
@@ -28,7 +21,6 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       render json: @user
@@ -37,18 +29,15 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
-
-    # Only allow a trusted parameter "white list" through.
+  
     def user_params
       params.require(:user).permit(:name, :username, :password_digest)
     end

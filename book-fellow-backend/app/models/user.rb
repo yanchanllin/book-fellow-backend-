9a29f5b53fd05cc6407d74c4ba9b2e2855c90bf2
yanchanllin <yonalin1@gmail.com>
2019-10-07 19:44:28 -0700

    delete spaces

--- book-fellow-backend/app/models/user.rb
+++ book-fellow-backend/app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
     has_secure_password 
-
     has_many :books
     has_many :reviews
-
-
     validates :name, :username, presence: true
     validates :username, uniqueness: true
+    
 end

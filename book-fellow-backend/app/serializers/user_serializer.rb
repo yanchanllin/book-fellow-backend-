98fea28ff8b14936c171f918dc264905619bd5f1
yanchanllin <yonalin1@gmail.com>
2019-10-07 19:47:18 -0700

    delete spaces

--- book-fellow-backend/app/serializers/user_serializer.rb
+++ book-fellow-backend/app/serializers/user_serializer.rb
@@ -1,12 +1,11 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username, :name
-  # has_many :books, serializer: BookSerializer
-
   attribute :books do |user|
     {
       books: user.books
     
     }
   end
+  
 end

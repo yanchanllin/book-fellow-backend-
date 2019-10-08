b03abb82ce6e3ed0998620ffbeeb230b7f6f2ce0
yanchanllin <yonalin1@gmail.com>
2019-10-07 19:49:18 -0700

    delete space in between

--- book-fellow-backend/config/routes.rb
+++ book-fellow-backend/config/routes.rb
@@ -3,15 +3,12 @@ Rails.application.routes.draw do
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   
-  #i want my api be bookfellow.com/api/v1/resource
   namespace :api do 
     namespace :v1 do 
       resources :users
       resources :books do 
         resources :reviews
-    end 
+     end 
     end   
   end 
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

3821a4de5bbbebded2e3fdd71a6b4713639285cf
yanchanlin <yonalin1@gmail.com>
2019-11-01 09:39:59 -0700

    fixing spacing

--- book-fellow-frontend/src/store.js
+++ book-fellow-frontend/src/store.js
@@ -15,7 +15,6 @@ const reducer = combineReducers({
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
